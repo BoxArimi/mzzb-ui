be65d8725cf54869992fd55344a9d3ceec94f122
fuhaiwei <fuhaiwei@126.com>
2018-02-13 21:28:56 +0800

    feat: add button to control collapsed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,68 @@
 import * as React from 'react';
 import './App.css';
+import { Icon, Layout } from 'antd';
 import { AdminSakura } from './components';
 
-class App extends React.Component<{}, {}> {
+interface AppState {
+  collapsed: boolean;
+}
+
+class App extends React.Component<{}, AppState> {
+
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      collapsed: false
+    };
+  }
+
+  onCollapse = (collapsed: boolean, type: 'clickTrigger' | 'responsive') => {
+    if (type === 'responsive') {
+      this.setState({...this.state, collapsed});
+    }
+  }
 
   render() {
     return (
-      <div className="App-root">
-        <AdminSakura/>
+      <div className="app-root">
+        <Layout>
+          <Layout.Sider
+            className="app-sider"
+            collapsed={this.state.collapsed}
+            onCollapse={this.onCollapse}
+            collapsedWidth={0}
+            breakpoint="md"
+            trigger={null}
+          >
+            sider
+          </Layout.Sider>
+          <Layout>
+            <Layout.Header
+              className="app-header"
+            >
+              <Icon
+                className="header-icon"
+                onClick={() => this.setState({...this.state, collapsed: !this.state.collapsed})}
+                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              />
+            </Layout.Header>
+            <Layout.Content
+              className="app-content"
+            >
+              <AdminSakura/>
+            </Layout.Content>
+            <Layout.Footer
+              className="app-footer"
+            >
+              footer
+            </Layout.Footer>
+          </Layout>
+        </Layout>
       </div>
     );
   }
-  
+
 }
 
 export default App;
