af64c69fa5098aa10de1ee90f10dcd0b750b9e2e
fuhaiwei <fuhaiwei@126.com>
2018-02-14 20:54:56 +0800

    refactor: move async() to index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,34 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import './index.css'
 
 import App from './App'
-import { async } from './lib/loading'
+import Loading from './lib/loading'
+import NotFound from './components/not-found'
+import * as Loadable from 'react-loadable'
 import registerServiceWorker from './registerServiceWorker'
+
+export const async = (loader: () => any) => {
+  return Loadable({
+    loader: loader,
+    loading: Loading,
+    delay: 300,
+    timeout: 5000,
+  })
+}
+
+const AsyncAdminSakura = async(() => import('./components/admin-sakura'))
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
       <Switch>
         <Redirect exact={true} path="/" to="/admin/sakura"/>
-        <Route path="/admin/sakura" component={async(() => import('./components/admin-sakura'))}/>
-        <Route path="/not-found" component={async(() => import('./components/not-found'))}/>
+        <Route path="/admin/sakura" component={AsyncAdminSakura}/>
+        <Route path="/not-found" component={NotFound}/>
         <Redirect exact={true} path="*" to="/not-found"/>
       </Switch>
     </App>
   </BrowserRouter>,
   document.getElementById('root') as HTMLElement
 )
+
 registerServiceWorker()
--- src/lib/loading/index.ts
+++ src/lib/loading/index.ts
@@ -1,13 +1,3 @@
-import * as Loadable from 'react-loadable'
 import { Loading } from './loading'
 
 export default Loading
-
-export const async = (loader: () => any) => {
-  return Loadable({
-    loader: loader,
-    loading: Loading,
-    delay: 300,
-    timeout: 5000,
-  })
-}
