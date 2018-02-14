f1583cdd1224c2de524fae35c63bf9e230fe01ec
fuhaiwei <fuhaiwei@126.com>
2018-02-14 22:06:08 +0800

    feat: add get /api/admin/sakuras api

--- src/components/admin-sakura/admin-sakura.tsx
+++ src/components/admin-sakura/admin-sakura.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Alert } from 'antd'
 import Table, { Column } from '../../lib/table'
 import './admin-sakura.css'
 
@@ -6,6 +7,7 @@ import { Manager, Model } from '../../utils/manager'
 import produce from 'immer'
 
 interface Sakura extends Model {
+  id: number
   key: string
   title: string
   enabled: boolean
@@ -17,9 +19,20 @@ interface AdminStateState {
   message?: string
 }
 
+const formatTime = (sakuraUpdateDate: number) => {
+  const millis = new Date().getTime() - sakuraUpdateDate
+  const minutes = Math.floor(millis / 60000)
+  const hour = Math.floor(minutes / 60)
+  const minute = Math.floor(minutes % 60)
+  return `${hour}时${minute}分前`
+}
+
 const columns: Column<Sakura>[] = [
-  {key: 'id', title: 'ID', format: (t) => t.id},
-  {key: 'title', title: 'Title', format: (t) => t.title},
+  {key: 'id', title: '#', format: (t) => t.id},
+  {key: 'key', title: 'Key', format: (t) => t.key},
+  {key: 'title', title: '标题', format: (t) => t.title},
+  {key: 'enabled', title: '启用', format: (t) => t.enabled ? '是' : '否'},
+  {key: 'sakuraUpdateDate', title: '上次更新', format: (t) => formatTime(t.sakuraUpdateDate)},
 ]
 
 export class AdminSakura extends React.Component<{}, AdminStateState> {
@@ -31,8 +44,8 @@ export class AdminSakura extends React.Component<{}, AdminStateState> {
   }
 
   async componentDidMount() {
-    const manager: Manager<Sakura> = new Manager('/api/sakuras')
-    const result = await manager.findAll('discColumns=id,title')
+    const manager: Manager<Sakura> = new Manager('/api/admin/sakuras')
+    const result = await manager.findAll()
     this.setState(produce(this.state, (draft: AdminStateState) => {
       if (result.success) {
         draft.sakuras = result.data
@@ -45,11 +58,13 @@ export class AdminSakura extends React.Component<{}, AdminStateState> {
   render() {
     return (
       <div className="App">
+        {this.state.message && (
+          <Alert message={this.state.message} type="error"/>
+        )}
         {this.state.sakuras && (
           <Table title="sakura list" rows={this.state.sakuras} columns={columns}/>
         )}
       </div>
     )
   }
-
 }
