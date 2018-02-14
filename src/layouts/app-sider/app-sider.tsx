58c0f5cc4ecbfbd4da9a5e2a8757daf0a7ade345
fuhaiwei <fuhaiwei@126.com>
2018-02-15 01:38:09 +0800

    fix: remove Reload before redirect to path

--- src/layouts/app-sider/app-sider.tsx
+++ src/layouts/app-sider/app-sider.tsx
@@ -28,6 +28,9 @@ export class AppSider extends React.Component<RouteComponentProps<{}>, {}> {
 
   onClickItem = ({key}: ClickParam) => {
     if (key.charAt(0) === '/') {
+      this.context.update((draft: AppState) => {
+        draft.reload = undefined
+      })
       this.props.history.push(key)
     } else {
       window.open(key)
