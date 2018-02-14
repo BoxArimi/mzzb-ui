2f4e919720ee9ef93888228adbeefd777ca11c17
fuhaiwei <fuhaiwei@126.com>
2018-02-14 21:47:16 +0800

    feat: add renderMenu to AppSider

--- src/layouts/app-sider/app-sider.tsx
+++ src/layouts/app-sider/app-sider.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
+import Icon from '../../lib/icon'
 
 import { AppContext, AppState } from '../../App'
 import { CollapseType } from 'antd/lib/layout/Sider'
+import { default as routes, RouteInfo } from '../../common/routes'
 import produce from 'immer'
+
+const renderTitle = (route: RouteInfo) => {
+  return (
+    <span><Icon className="sider-icon" type={route.icon}/>{route.title}</span>
+  )
+}
+
+const renderMenu = (route: RouteInfo, key: number): React.ReactNode => {
+  return route.hasRoutes ? (
+    <Menu.SubMenu key={key} title={renderTitle(route)}>
+      {route.routes.map(renderMenu)}
+    </Menu.SubMenu>
+  ) : (
+    <Menu.Item key={key}>
+      {renderTitle(route)}
+    </Menu.Item>
+  )
+}
 
 export class AppSider extends React.Component<AppContext, {}> {
 
@@ -25,7 +45,16 @@ export class AppSider extends React.Component<AppContext, {}> {
         breakpoint="md"
         trigger={null}
       >
-        sider
+        <div className="sider-logo">
+          <h2>名作之壁</h2>
+        </div>
+        <Menu
+          mode="inline"
+          selectedKeys={[document.location.pathname]}
+          style={{height: '100%'}}
+        >
+          {routes.map(renderMenu)}
+        </Menu>
       </Layout.Sider>
     )
   }
