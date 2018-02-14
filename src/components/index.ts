4c411e7d680990c62f17efbe12533f7523449db6
fuhaiwei <fuhaiwei@126.com>
2018-02-14 14:48:37 +0800

    feat: add Loadable and use for Route

--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { AdminSakura } from './admin-sakura/admin-sakura'
-export { NotFound } from './not-found/not-found'
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,29 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
+import * as Loadable from 'react-loadable'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
 import App from './App'
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
-import { AdminSakura, NotFound } from './components'
+import Loading from './components/loading'
+
+const async = (loader: () => any) => {
+  return Loadable({
+    loader: loader,
+    loading: Loading,
+    delay: 300,
+    timeout: 5000,
+  })
+}
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
       <Switch>
         <Redirect exact={true} path="/" to="/admin/sakura"/>
-        <Route path="/admin/sakura" component={AdminSakura}/>
-        <Route path="/not-found" component={NotFound}/>
+        <Route path="/admin/sakura" component={async(() => import('./components/admin-sakura'))}/>
+        <Route path="/not-found" component={async(() => import('./components/not-found'))}/>
         <Redirect exact={true} path="*" to="/not-found"/>
       </Switch>
     </App>
