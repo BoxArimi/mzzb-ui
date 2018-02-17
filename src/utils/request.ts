c0a44f5591940a57dc9f7de328dd8b303b72533d
fuhaiwei <fuhaiwei@126.com>
2018-02-17 23:48:09 +0800

    feat: Change respone first parse json

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -20,13 +20,6 @@ function prepareHeaders({headers = {}, ...prors}: RequestInit) {
   return {headers, ...prors}
 }
 
-function checkStatus(response: Response) {
-  if (response.status < 200 || response.status > 300) {
-    throw new Error(`服务器未正确响应: ${response.status}: ${response.statusText}`)
-  }
-  return response
-}
-
 function saveCsrfToken(response: Response) {
   const headers = response.headers
   sessionStorage[header] = headers.get(header)
@@ -38,7 +31,7 @@ function parseToJSON(response: Response) {
   try {
     return response.json()
   } catch (error) {
-    throw new Error(`解析JSON遇到错误: ${error.message}`)
+    throw new Error(`服务器未正确响应: ${response.status}: ${response.statusText}`)
   }
 }
 
@@ -46,7 +39,6 @@ export default function request(url: string, props: RequestInit = {}) {
   props = prepareCookies(props)
   props = prepareHeaders(props)
   return fetch(url, props)
-    .then(checkStatus)
     .then(saveCsrfToken)
     .then(parseToJSON)
     .catch(err => {
